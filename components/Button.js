7eeedf234438bfebcdd713d9773dfa141ce5ec7f
Erick Camacho <ercamdev@gmail.com>
2021-02-24 21:55:05 -0600

    Fix Button component bug

--- components/Button.js
+++ components/Button.js
@@ -1,10 +1,16 @@
 const Button = ({ children, primary, href = "#" }) => {
   return (
     <div className="transform hover:-translate-y-1 inline-block">
-      <a href={href} role="button" className={primary ? 
-        "uppercase bg-yellow-500 text-white border-2 border-yellow-500 py-2 px-8 lg:px-9 text-xs font-bold rounded-full mt-24 shadow-md transform hover:-translate-y-1":
-        "uppercase text-yellow-500 border-2 border-yellow-500 py-2  px-8 lg:px-9 text-xs font-bold rounded-full mt-24 shadow-md transform hover:-translate-y-1"
-      }>
+      <a 
+        href={href} 
+        role="button" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        className={primary ? 
+          "uppercase bg-yellow-500 text-white border-2 border-yellow-500 py-2 px-8 lg:px-9 text-xs font-bold rounded-full mt-24 shadow-md transform hover:-translate-y-1":
+          "uppercase text-yellow-500 border-2 border-yellow-500 py-2  px-8 lg:px-9 text-xs font-bold rounded-full mt-24 shadow-md transform hover:-translate-y-1"
+        }
+      >
         { children }
       </a>
     </div>
